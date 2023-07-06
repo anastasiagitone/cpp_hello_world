8d88c72ec8b5403224d0b699a5b756fedcbdbec3
anastasiagitone <135426076+anastasiagitone@users.noreply.github.com>
2023-07-06 12:17:20 +0300

    Create test.sh

---
+++ tests/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+g++ app.cpp -o app
+
+output=$(./app)
+
+if [ "$output" = "05.07.2023" ]
+then
+  echo "Вывод программы совпадает с ожидаемым: $output"
+else
+  echo "Вывод программы не совпадает с ожидаемым. Ожидается: $expected_output, получено: $output"
+fi
